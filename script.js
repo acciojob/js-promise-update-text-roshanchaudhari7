4a3092f2cafc3b824fd917d0a760998d8be6f705
Roshan Chaudhari <98824802+roshanchaudhari7@users.noreply.github.com>
2025-04-15 20:08:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function getTextMessage() {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve("Hello, world!");
+		}, 1000);
+	});
+}
+
+getTextMessage().then((message) => {
+	document.getElementById("output").textContent = message;
+});
